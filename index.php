79c1bee9a1f0a5cbbc61c74672a17daa01d7e6cf
rafalcicirko <rafal.cicirko@secretsales.com>
2016-09-28 12:14:48 +0100

    add fb login page

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
-$token = "";
-
 require_once __DIR__.'/vendor/autoload.php';
 
 $config = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__.'/config.yml'));
+
+$loader = new Twig_Loader_Filesystem(__DIR__.'/html');
+$twig = new Twig_Environment($loader);
+
+if (!isset($_POST['token'])) {
+    echo $twig->render("intro.html.twig", ['appId' => $config['parameters']['id']]);
+    exit;
+}
+
+$token = $_POST['token'];
 
 $face = new Facebook\Facebook([
     'app_id' => $config['parameters']['id'],
@@ -13,8 +21,11 @@ $face = new Facebook\Facebook([
 ]);
 
 $timeline = new \App\Timeline($face);
-$results = $timeline->generate($token);
+try {
+    $results = $timeline->generate($token);
+} catch (\Facebook\Exceptions\FacebookAuthenticationException $e) {
+    echo "Problem with token.\n";
+    exit;
+}
 
-$loader = new Twig_Loader_Filesystem(__DIR__.'/html');
-$twig = new Twig_Environment($loader);
 echo $twig->render("content.html.twig", ['results' => $results]);
